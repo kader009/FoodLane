8555ae8d29a8e7f10c3472273cb9704b8ace36a1
kader009 <kadermollarasel000@gmail.com>
2024-08-31 09:42:24 +0600

    auth api create

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+
+
+const AuthContext = createContext();
+const AuthProvider = ({children}) => {
+  return (
+    <AuthContext.Provider>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
