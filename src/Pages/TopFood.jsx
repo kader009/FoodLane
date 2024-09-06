fbc3aad5b3fb40e6d30b97ccfa26cdac3702c56f
kader009 <kadermollarasel000@gmail.com>
2024-09-06 22:32:28 +0600

    loading spinner add for data loading

--- src/Pages/TopFood.jsx
+++ src/Pages/TopFood.jsx
@@ -2,17 +2,23 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Container from '../Components/Container';
+import LoadingSpinner from '../Components/LoadingSpinner';
 
 const TopFood = () => {
   const [foods, SetFoods] = useState([]);
+  const [loading, SetLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get('http://localhost:5000/foodData')
       .then((res) => {
         SetFoods(res.data.foods || []);
+        SetLoading(false);
       })
-      .catch((error) => console.error('Error fetching food items:', error));
+      .catch((error) => {
+        console.error('Error fetching food items:', error);
+        SetLoading(false);
+      });
   }, []);
 
   return (
@@ -23,53 +29,59 @@ const TopFood = () => {
         flavor. Savor <br /> a culinary journey that delights every bite.
       </p>
 
-      <Container>
-        <div className=" flex items-center justify-center flex-wrap gap-4 ">
-          {foods.slice(0, 6).map((food) => (
-            <div
-              key={food._id}
-              className="w-96 mx-auto bg-white rounded-xl shadow-md overflow-hidden transform transition-transform hover:scale-105 duration-300 mb-3"
-            >
-              <img
-                src={food.foodImage}
-                alt={food.foodName}
-                className="w-full h-48 object-cover"
-                loading="lazy"
-              />
-              <div className="p-4">
-                <h3 className="text-xl font-bold text-gray-800 mb-2">
-                  {food.foodName}
-                </h3>
-                <p className="text-gray-600 text-sm mb-4">{food.description}</p>
-                <div className="flex justify-between items-center">
-                  <span className="text-xl font-bold text-gray-800">
-                    ${food.price}
-                  </span>
-                  <button className="flex items-center bg-[#F44336] text-white px-4 py-2 rounded hover:bg-[#D32F2F] transition-colors">
-                    <Link to={`/single-food/${food._id}`}>Detail</Link>
-                    <div className="ms-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-                        />
-                      </svg>
-                    </div>
-                  </button>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Container>
+          <div className=" flex items-center justify-center flex-wrap gap-4 ">
+            {foods.slice(0, 6).map((food) => (
+              <div
+                key={food._id}
+                className="w-96 mx-auto bg-white rounded-xl shadow-md overflow-hidden transform transition-transform hover:scale-105 duration-300 mb-3"
+              >
+                <img
+                  src={food.foodImage}
+                  alt={food.foodName}
+                  className="w-full h-48 object-cover"
+                  loading="lazy"
+                />
+                <div className="p-4">
+                  <h3 className="text-xl font-bold text-gray-800 mb-2">
+                    {food.foodName}
+                  </h3>
+                  <p className="text-gray-600 text-sm mb-4">
+                    {food.description}
+                  </p>
+                  <div className="flex justify-between items-center">
+                    <span className="text-xl font-bold text-gray-800">
+                      ${food.price}
+                    </span>
+                    <button className="flex items-center bg-[#F44336] text-white px-4 py-2 rounded hover:bg-[#D32F2F] transition-colors">
+                      <Link to={`/single-food/${food._id}`}>Detail</Link>
+                      <div className="ms-2">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                          />
+                        </svg>
+                      </div>
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </Container>
+            ))}
+          </div>
+        </Container>
+      )}
 
       <div className="text-center py-6">
         <Link to={'/all-foods'}>
--- src/Pages/profile/MyOrder.jsx
+++ src/Pages/profile/MyOrder.jsx
@@ -1,22 +1,33 @@
 import { useState, useEffect } from 'react';
 import useAuth from '../../Hooks/useAuth';
-
+import LoadingSpinner from '../../Components/LoadingSpinner';
 
 const MyOrder = () => {
   const { user } = useAuth();
-  const [foods, SetFood] = useState([]);
+  const [foods, setFoods] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/orders?email=${user?.email}`, {
-      credentials: 'include',
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        SetFood(data);
+    const fetchOrders = () => {
+      fetch(`http://localhost:5000/orders?email=${user?.email}`, {
+        credentials: 'include',
       })
-      .catch((error) => {
-        console.log(error);
-      });
+        .then((res) => res.json())
+        .then((data) => {
+          setFoods(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoading(false);
+        });
+    };
+
+    if (user?.email) {
+      fetchOrders();
+    } else {
+      setLoading(false);
+    }
   }, [user?.email]);
 
   const handleDelete = (id) => {
@@ -26,7 +37,7 @@ const MyOrder = () => {
       .then((res) => res.json())
       .then((data) => {
         if (data.deletedCount > 0) {
-          SetFood(foods.filter((food) => food._id !== id));
+          setFoods(foods.filter((food) => food._id !== id));
         }
       })
       .catch((error) => {
@@ -37,48 +48,53 @@ const MyOrder = () => {
   return (
     <div className="mx-4 my-12">
       <h1 className="text-2xl font-bold mb-4 text-white">My Orders</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center">
-        {foods.map((food) => (
-          <div
-            key={food._id}
-            className="bg-white text-black rounded-lg p-4 shadow-lg flex flex-col flex-wrap items-center"
-          >
-            <img
-              src={food.foodImage}
-              alt={food.foodName}
-              className="w-32 h-32 object-cover rounded-full mb-4"
-            />
-            <h2 className="text-xl font-semibold mb-2">{food.foodName}</h2>
-            <p className="mb-2">$ {food.price}</p>
-            <p className="text-sm mb-2">{food.Date}</p>
-            <p className="text-sm mb-4">Owner: {food.buyerName}</p>
-            <button
-              onClick={() => handleDelete(food._id)}
-              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center">
+          {foods.map((food) => (
+            <div
+              key={food._id}
+              className="bg-white text-black rounded-lg p-4 shadow-lg flex flex-col flex-wrap items-center"
             >
-              <div className="flex">
-                <div>Delete</div>
-                <div className="ms-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="size-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
-                    />
-                  </svg>
+              <img
+                src={food.foodImage}
+                alt={food.foodName}
+                className="w-32 h-32 object-cover rounded-full mb-4"
+              />
+              <h2 className="text-xl font-semibold mb-2">{food.foodName}</h2>
+              <p className="mb-2">$ {food.price}</p>
+              <p className="text-sm mb-2">{food.Date}</p>
+              <p className="text-sm mb-4">Owner: {food.buyerName}</p>
+              <button
+                onClick={() => handleDelete(food._id)}
+                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+              >
+                <div className="flex">
+                  <div>Delete</div>
+                  <div className="ms-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                      />
+                    </svg>
+                  </div>
                 </div>
-              </div>
-            </button>
-          </div>
-        ))}
-      </div>
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
