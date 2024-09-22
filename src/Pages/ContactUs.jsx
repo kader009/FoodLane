edaebf9c6ae1b5dd0e9ec6f792e7df748c3fba58
kader009 <kadermollarasel000@gmail.com>
2024-09-22 20:22:34 +0600

    fix some error

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -21,7 +21,6 @@ const ContactUs = () => {
     }
 
     console.log('Form submitted:', form);
-    // Implement form submission logic here
   };
 
   return (
@@ -115,13 +114,13 @@ const ContactUs = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="size-6"
+                  className="size-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
                   />
                 </svg>
