3781a4c1df2afc7c8c1a3c9c21c0343d83613fb0
kader009 <kadermollarasel000@gmail.com>
2024-09-27 10:54:20 +0600

    order page design modify

--- src/Pages/profile/MyOrder.jsx
+++ src/Pages/profile/MyOrder.jsx
@@ -62,71 +62,77 @@ const MyOrder = () => {
   };
 
   return (
-    <div className="mx-4 my-12">
-      <h1 className="text-3xl font-black text-[#F44336] text-center">
-        My Orders
-      </h1>
-      <p className="capitalize text-lg mb-4 mt-2 text-center">
-        My order list of food items
-      </p>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <div className="max-w-screen-lg mx-auto py-8">
+          <h1 className="text-3xl font-black text-[#F44336] text-center">
+            My Orders
+          </h1>
+          <p className="capitalize text-lg mb-4 mt-2 text-center">
+            My order list of food items
+          </p>
 
-      {loading ? (
-        <LoadingSpinner />
-      ) : (
-        <>
-          {foods?.length === 0 ? (
-            <p className="text-center text-2xl mt-6">No Data Found</p>
+          {loading ? (
+            <LoadingSpinner />
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-              {foods?.map((food) => (
-                <div
-                  key={food._id}
-                  className="bg-white text-black rounded-lg p-4 shadow-lg flex flex-col flex-wrap justify-center items-center"
-                >
-                  <img
-                    src={food.foodImage}
-                    alt={food.foodName}
-                    className="w-32 h-32 object-cover rounded-full mb-4"
-                  />
-                  <h2 className="text-xl font-semibold mb-2">
-                    {food.foodName}
-                  </h2>
-                  <p className="mb-2 text-[#F44336] font-bold">${food.price}</p>
-                  <p className="text-sm mb-2">{food.Date}</p>
-                  <p className="text-sm mb-4">
-                    <span className="font-bold">Owner: </span>
-                    {food.buyerName}
-                  </p>
-                  <button
-                    onClick={() => handleDelete(food._id)}
-                    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-                  >
-                    <div className="flex">
-                      <div>Delete</div>
-                      <div className="ms-2">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
-                          />
-                        </svg>
-                      </div>
+            <>
+              {foods?.length === 0 ? (
+                <p className="text-center text-2xl mt-6">No Data Found</p>
+              ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-2 lg:px-0">
+                  {foods?.map((food) => (
+                    <div
+                      key={food._id}
+                      className="bg-white text-black rounded-lg p-4 shadow-lg flex flex-col items-center"
+                    >
+                      <img
+                        src={food.foodImage}
+                        alt={food.foodName}
+                        className="w-32 h-32 object-cover rounded-full mb-4"
+                      />
+                      <h2 className="text-xl font-semibold mb-2">
+                        {food.foodName}
+                      </h2>
+                      <p className="mb-2 text-[#F44336] font-bold">
+                        ${food.price}
+                      </p>
+                      <p className="text-sm mb-2">{food.Date}</p>
+                      <p className="text-sm mb-4">
+                        <span className="font-bold">Owner: </span>
+                        {food.buyerName}
+                      </p>
+                      <button
+                        onClick={() => handleDelete(food._id)}
+                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                      >
+                        <div className="flex">
+                          <div>Delete</div>
+                          <div className="ms-2">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                      </button>
                     </div>
-                  </button>
+                  ))}
                 </div>
-              ))}
-            </div>
+              )}
+            </>
           )}
-        </>
-      )}
+        </div>
+      </div>
     </div>
   );
 };
