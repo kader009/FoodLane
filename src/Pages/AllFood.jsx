54a731f26a9c3cebcc0b0cc14759a1c023e7e389
kader009 <kadermollarasel000@gmail.com>
2025-06-19 20:16:01 +0600

    all food button icon add

--- src/Pages/AllFood.jsx
+++ src/Pages/AllFood.jsx
@@ -85,11 +85,11 @@ const AllFood = () => {
               </div>
             ) : (
               <Container>
-                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 justify-center items-center justify-items-center">
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 justify-center items-center justify-items-center">
                   {filteredItems.map((food) => (
                     <div
                       key={food._id}
-                      className="w-[22rem] rounded overflow-hidden shadow-lg bg-white text-black mx-8 mb-6"
+                      className="w-[24rem] rounded overflow-hidden shadow-lg bg-white text-black mx-8 mb-6"
                     >
                       <img
                         className="w-full h-48 object-cover"
@@ -111,8 +111,24 @@ const AllFood = () => {
                         </p>
                       </div>
                       <div className="px-4 pb-4 flex justify-start">
-                        <button className="bg-[#F44336] hover:bg-[#D32F2F] text-white font-bold py-2 px-6 rounded w-full md:w-auto ">
+                        <button className="flex items-center bg-[#F44336] text-white px-4 py-2 rounded hover:bg-[#D32F2F] transition-colors">
                           <Link to={`/single-food/${food._id}`}>Detail</Link>
+                          <div className="ms-2">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="size-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                              />
+                            </svg>
+                          </div>
                         </button>
                       </div>
                     </div>
