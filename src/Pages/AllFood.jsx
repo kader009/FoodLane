255727a6ca9f5f46db4d3f44942707ae0947ceef
kader009 <kadermollarasel000@gmail.com>
2025-06-26 08:14:02 +0600

    tablet responsive issue solve

--- src/Pages/AllFood.jsx
+++ src/Pages/AllFood.jsx
@@ -14,9 +14,7 @@ const AllFood = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(
-        `https://foodlane-server-api.onrender.com/foodData?page=${page}&limit=6`
-      )
+      .get(`https://foodlane-server-api.onrender.com/foodData?page=${page}&limit=6`)
       .then((response) => {
         setFoodItems(response.data.foods);
         setTotalPages(response.data.totalPages);
@@ -50,113 +48,111 @@ const AllFood = () => {
         <title>FoodLane | AllFood</title>
       </Helmet>
 
-      <div className="bg-black">
-        <div className=" bg-black text-white">
-          <header className=" py-4 text-center">
-            <h1 className="text-4xl font-black text-[#F44336]">
-              Food Listing Page
-            </h1>
-            <p className="capitalize mt-3 text-lg">
-              We provide the best food items for our customers
-            </p>
-          </header>
+      <div className="bg-black text-white">
+        <header className="py-4 text-center">
+          <h1 className="text-4xl font-black text-[#F44336]">
+            Food Listing Page
+          </h1>
+          <p className="capitalize mt-3 text-lg">
+            We provide the best food items for our customers
+          </p>
+        </header>
 
-          <section className="container mx-auto py-2">
-            <div className="mb-6 text-center mx-2">
-              <input
-                type="text"
-                placeholder="Search for food..."
-                className="w-full max-w-lg px-4 py-2 text-black rounded-md mb-4"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-              />
+        <section className="container mx-auto py-4 px-2">
+          <div className="mb-6 text-center">
+            <input
+              type="text"
+              placeholder="Search for food..."
+              className="w-full max-w-lg px-4 py-2 text-black rounded-md mb-4"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+
+          {/* Loading Spinner */}
+          {loading ? (
+            <div className="flex justify-center items-center">
+              <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full border-t-[#F44336]"></div>
             </div>
-
-            {/* Loading Spinner */}
-            {loading ? (
-              <div className="flex justify-center items-center">
-                <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full border-t-[#F44336]"></div>
-              </div>
-            ) : filteredItems.length === 0 ? (
-              <div className="flex justify-center items-center">
-                <p className="text-white text-lg">
-                  No food items found for {searchQuery}
-                </p>
-              </div>
-            ) : (
-              <Container>
-                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 justify-center items-center justify-items-center">
-                  {filteredItems.map((food) => (
-                    <div
-                      key={food._id}
-                      className="w-[24rem] rounded overflow-hidden shadow-lg bg-white text-black mx-8 mb-6"
-                    >
-                      <img
-                        className="w-full h-48 object-cover"
-                        src={food.foodImage}
-                        alt={food.foodName}
-                      />
-                      <div className="px-4 py-6">
-                        <div className="font-bold text-xl mb-2">
-                          {food.foodName}
-                        </div>
-                        <p className="text-base">
-                          Category: {food.foodCategory}
-                        </p>
-                        <p className="font-bold mt-2 text-[#F44336]">
-                          Price: ${food.price}
-                        </p>
-                        <p className="text-sm mt-1">
-                          Quantity: {food.quantity}
-                        </p>
+          ) : filteredItems.length === 0 ? (
+            <div className="flex justify-center items-center">
+              <p className="text-white text-lg">
+                No food items found for {searchQuery}
+              </p>
+            </div>
+          ) : (
+            <Container>
+              <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4">
+                {filteredItems.map((food) => (
+                  <div
+                    key={food._id}
+                    className="w-full max-w-sm mx-auto rounded overflow-hidden shadow-lg bg-white text-black"
+                  >
+                    <img
+                      className="w-full h-48 object-cover"
+                      src={food.foodImage}
+                      alt={food.foodName}
+                    />
+                    <div className="px-4 py-6">
+                      <div className="font-bold text-xl mb-2">
+                        {food.foodName}
                       </div>
-                      <div className="px-4 pb-4 flex justify-start">
-                        <button className="flex items-center bg-[#F44336] text-white px-4 py-2 rounded hover:bg-[#D32F2F] transition-colors">
-                          <Link to={`/single-food/${food._id}`}>Detail</Link>
-                          <div className="ms-2">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                              className="size-6"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-                              />
-                            </svg>
-                          </div>
-                        </button>
-                      </div>
+                      <p className="text-base">
+                        Category: {food.foodCategory}
+                      </p>
+                      <p className="font-bold mt-2 text-[#F44336]">
+                        Price: ${food.price}
+                      </p>
+                      <p className="text-sm mt-1">
+                        Quantity: {food.quantity}
+                      </p>
                     </div>
-                  ))}
-                </div>
-              </Container>
-            )}
+                    <div className="px-4 pb-4 flex justify-start">
+                      <button className="flex items-center bg-[#F44336] text-white px-4 py-2 rounded hover:bg-[#D32F2F] transition-colors">
+                        <Link to={`/single-food/${food._id}`}>Detail</Link>
+                        <div className="ms-2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                            />
+                          </svg>
+                        </div>
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </Container>
+          )}
 
-            {/* Pagination Controls */}
-            <div className="flex justify-center items-center my-4">
-              <button
-                onClick={handlePreviousPage}
-                disabled={page === 1}
-                className="bg-[#F44336] text-white px-4 py-2 rounded mx-2 disabled:opacity-50"
-              >
-                Previous
-              </button>
-              <span>{`Page ${page} of ${totalPages}`}</span>
-              <button
-                onClick={handleNextPage}
-                disabled={page === totalPages}
-                className="bg-[#F44336] text-white px-4 py-2 rounded mx-2 disabled:opacity-50"
-              >
-                Next
-              </button>
-            </div>
-          </section>
-        </div>
+          {/* Pagination Controls */}
+          <div className="flex justify-center items-center my-6 flex-wrap gap-4">
+            <button
+              onClick={handlePreviousPage}
+              disabled={page === 1}
+              className="bg-[#F44336] text-white px-4 py-2 rounded disabled:opacity-50"
+            >
+              Previous
+            </button>
+            <span className="text-white">{`Page ${page} of ${totalPages}`}</span>
+            <button
+              onClick={handleNextPage}
+              disabled={page === totalPages}
+              className="bg-[#F44336] text-white px-4 py-2 rounded disabled:opacity-50"
+            >
+              Next
+            </button>
+          </div>
+        </section>
       </div>
     </div>
   );
