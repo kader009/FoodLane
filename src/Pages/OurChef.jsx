ee591a7826675de404a3f4afd5bce3fc901f0cc8
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2025-08-31 15:54:02 +0600

    chef section mobile device responsive issue solve

--- src/Pages/OurChef.jsx
+++ src/Pages/OurChef.jsx
@@ -26,15 +26,14 @@ const chefs = [
   {
     name: 'Erik Rowan',
     designation: 'Pizza Chef',
-    image:
-      'https://images.pexels.com/photos/8093867/pexels-photo-8093867.jpeg',
+    image: 'https://images.pexels.com/photos/8093867/pexels-photo-8093867.jpeg',
     rating: 4.5,
   },
 ];
 
 const OurChefs = () => {
   return (
-    <section className="py-12 bg-black px-12">
+    <section className="py-12 bg-black px-4 sm:px-6 md:px-12">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-black mb-2 text-[#F44336]">
           Meet Our Chefs
@@ -46,7 +45,10 @@ const OurChefs = () => {
         <Container>
           <div className="flex flex-wrap justify-center">
             {chefs.map((chef, index) => (
-              <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-8">
+              <div
+                key={index}
+                className="w-full sm:w-1/2 lg:w-1/3 px-2 sm:px-4 mb-8"
+              >
                 <div className="bg-gray-950 rounded-lg shadow-lg p-6 hover:shadow-xl transition-shadow duration-300 ease-in-out">
                   <img
                     src={chef.image}
@@ -57,7 +59,6 @@ const OurChefs = () => {
                     {chef.name}
                   </h3>
                   <p className="text-gray-400 mb-3">{chef.designation}</p>
-                  {/* Chef rating system */}
                   <div className="flex justify-center mb-4">
                     {[...Array(5)].map((star, i) => (
                       <FaStar
