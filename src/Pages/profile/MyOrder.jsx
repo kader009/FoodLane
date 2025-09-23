0b3b5a624cc9f05a0d275250155af68d62ed5280
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2025-09-23 19:38:08 +0600

    order page card responsive issue solve

--- src/Pages/profile/MyOrder.jsx
+++ src/Pages/profile/MyOrder.jsx
@@ -108,7 +108,7 @@ const MyOrder = () => {
                       </div>
                     </div>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-4 lg:px-0 mb-10">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4 lg:px-0 mb-10">
                       {foods?.map((food) => (
                         <div
                           key={food._id}
