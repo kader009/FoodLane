753deb89ceba280a97201484d0a338bf1906d027
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2026-01-02 10:07:56 +0600

    lazy loading added for better performance

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import { Helmet } from 'react-helmet-async';
-import About from '../About/About';
+import { lazy, Suspense } from 'react';
+
 import Banner from '../Banner';
-import ContactUs from '../ContactUs';
-import OurChefs from '../OurChef';
+import About from '../About/About';
 import TopFood from '../TopFood';
-import TestimonialsSlider from '../../Components/TestimonialsSlider';
-import PricingSection from '../PricingSection';
+
+const OurChefs = lazy(() => import('../OurChef'));
+const TestimonialsSlider = lazy(() =>
+  import('../../Components/TestimonialsSlider')
+);
+const PricingSection = lazy(() => import('../PricingSection'));
+const ContactUs = lazy(() => import('../ContactUs'));
 
 const Home = () => {
   return (
@@ -18,10 +23,14 @@ const Home = () => {
         <Banner />
         <About />
         <TopFood />
-        <OurChefs />
-        <TestimonialsSlider />
-        <PricingSection />
-        <ContactUs />
+
+        {/* React.lazy() splits code, but loads immediately without viewport detection */}
+        <Suspense fallback={<div className="h-96 animate-pulse bg-gray-800" />}>
+          <OurChefs />
+          <TestimonialsSlider />
+          <PricingSection />
+          <ContactUs />
+        </Suspense>
       </main>
     </>
   );
