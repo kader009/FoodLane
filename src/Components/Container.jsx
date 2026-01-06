ffe11ff88697cd64a0dadf5c24005f818b844b6f
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2026-01-06 11:55:59 +0600

    folder name camelcase issue solve

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,9 +1,15 @@
+import PropTypes from 'prop-types';
+
 const Container = ({ children }) => {
   return (
     <div className=" w-full mx-auto max-w-[1276px] h-full px-[20px]">
       {children}
     </div>
   );
+};
+
+Container.propTypes = {
+  children: PropTypes.node
 };
 
 export default Container;
