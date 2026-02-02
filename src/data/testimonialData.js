4550b4b300e6260e03d808da4ca8170343e7ed6c
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2026-02-02 09:25:16 +0600

    data restructure for slider and create anohter folder for reuse

---
+++ src/data/testimonialData.js
@@ -0,0 +1,30 @@
+export const testimonials = [
+  {
+    name: 'Michael Anderson',
+    image: 'https://randomuser.me/api/portraits/men/44.jpg',
+    feedback:
+      'FoodLane is fantastic! The food quality is consistent, and their service is impeccable. Highly recommended!',
+    rating: 5,
+  },
+  {
+    name: 'Sophia Williams',
+    image: 'https://randomuser.me/api/portraits/women/65.jpg',
+    feedback:
+      'A remarkable dining experience! Fresh ingredients, amazing flavors, and excellent service every time!',
+    rating: 4,
+  },
+  {
+    name: 'David Johnson',
+    image: 'https://randomuser.me/api/portraits/men/85.jpg',
+    feedback:
+      'The dishes at FoodLane are crafted with such care! A perfect spot for a great meal with family and friends.',
+    rating: 5,
+  },
+  {
+    name: 'Emma Thompson',
+    image: 'https://randomuser.me/api/portraits/women/13.jpg',
+    feedback:
+      "FoodLane offers an amazing dining experience with a cozy ambiance. Highly recommend the chef's special!",
+    rating: 4,
+  },
+];
